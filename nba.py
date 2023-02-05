737ef5528cc93a5887071b9f6122190bc52186dd
Blizzcane <blizzcane@gmail.com>
2023-02-04 21:27:08 -0600

    testing nba api

--- nba.py
+++ nba.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask 
+from nba_api.live.nba.endpoints import scoreboard
 
+# Today's Score Board
+games = scoreboard.ScoreBoard()
+
+gameData = games.get_json()
+
+print(gameData)
 app = Flask(__name__)
 
 @app.route("/")
