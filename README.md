123c504f1e8727d45aa38c34efd763a40410d503
Blizzcane <blizzcane@gmail.com>
2023-02-04 21:51:37 -0600

    edited readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# nba-server
-# nba-server
+# NBA Game Score Server
+This is a Flask-based web server that displays the latest scores of NBA games. The scores are fetched from the nba_api and are updated every minute.
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+You will need to have flask and nba_api installed in your python environment. You can install them by running:
+
+```
+pip install flask nba_api
+
+```
+
+### Running the Server
+Clone the repository and navigate to the project directory. Then run the following command to start the server:
+
+```
+flask --app nba run
+```
+The server will start at http://127.0.0.1:5000/ by default.
+
+## Built With
+- Flask - The web framework used
+- nba_api - API for NBA data
+
+## Authors
+- OpenAI - Initial work
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
--- nba.py
+++ nba.py
@@ -1,28 +1,27 @@
-from flask import Flask 
-from nba_api.live.nba.endpoints import scoreboard
-
-# Query nba.live.endpoints.scoreboard and  list games in localTimeZone
-from datetime import datetime, timezone
-from dateutil import parser
+from flask import Flask, render_template
 from nba_api.live.nba.endpoints import scoreboard, boxscore
 
-# f = "{awayTeam} vs. {homeTeam}" 
-
-# board = scoreboard.ScoreBoard()
-# print("ScoreBoardDate: " + board.score_board_date)
-# games = board.games.get_dict()
-# for game in games:
-#     print("")
-#     print(f.format(gameId=game['gameId'], awayTeam=game['awayTeam']['teamName'], homeTeam=game['homeTeam']['teamName']))
-#     box = boxscore.BoxScore(game['gameId']) 
-#     currentScore = "{homeScore} - {awayScore}"
-    
-#     print(currentScore.format(homeScore=box.get_dict()['game']['homeTeam']['score'],awayScore=box.get_dict()['game']['awayTeam']['score']))
-#     print(game['gameStatusText'])
-    
- 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def show_games():
+    board = scoreboard.ScoreBoard()
+    games = board.games.get_dict()
+    games_list = []
+    for game in games:
+        box = boxscore.BoxScore(game['gameId']) 
+        current_score = "{homeScore} - {awayScore}".format(
+            homeScore=box.get_dict()['game']['homeTeam']['score'],
+            awayScore=box.get_dict()['game']['awayTeam']['score'])
+        game_dict = {
+            'gameId': game['gameId'],
+            'awayTeam': game['awayTeam']['teamName'],
+            'homeTeam': game['homeTeam']['teamName'],
+            'currentScore': current_score,
+            'gameStatusText': game['gameStatusText']
+        }
+        games_list.append(game_dict)
+    return render_template('games.html', games=games_list)
+
+if __name__ == "__main__":
+    app.run()
